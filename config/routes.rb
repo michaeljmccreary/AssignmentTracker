4143c4c2798e494025e7701d3e0eeb2502715c09
Michael McCreary <michaelmccreary95@gmail.com>
2025-11-04 20:44:36 +0000

    Using devise, I have now set up user sign-up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "pages/home"
+
+  root "pages#home"
+  # Creates devise routes, such as sign-in, sign-up etc. 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
