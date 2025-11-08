79d11e6e88fae9780b88c4c75436dc755c8ba846
Michael McCreary <michaelmccreary95@gmail.com>
2025-11-08 19:31:38 +0000

    adding functionality to change what is displayed dpending if a user is signed in or not

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      @assignments = current_user.classrooms.includes(:assignments).flat_map(&:assignments).sort_by(&:due_date)
   end
+end
 end
