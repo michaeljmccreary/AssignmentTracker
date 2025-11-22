0de8e919735bd4fac010b5bde6cc5522f89959a0
Michael McCreary <michaelmccreary95@gmail.com>
2025-11-22 15:27:06 +0000

    adding calander functionality to the app to display deadlines on a calander

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       @assignments = current_user.classrooms.includes(:assignments).flat_map(&:assignments).sort_by(&:due_date)
+    end
   end
-end
 end
